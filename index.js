eab5f5b0ed8176836bf73a9ce4c38a0c51ff6ef5
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-10-26 08:32:19 -0400

    added module.exports ot index.js and updated version semver to 0.1.0

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-
-  //Returns content processed for a palindrome testing.
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
     return this.content.toLowerCase();
   }
 
-  // Returns true for a palindrome, false otherwise.
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return processedContent === this.processedContent().reverse();
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
 
